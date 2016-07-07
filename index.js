41e7e917c57d4b66cc92cc1538f1847a80cf7c74
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-07-07 14:29:07 +0100

    modularising startup

--- index.js
+++ index.js
@@ -1,51 +1,11 @@
 const system = require('./lib/system')
-const get = require('lodash.get')
-const duration = require('parse-duration')
-const format = require('util').format
-const domain = require('domain').create()
-const signals = ['SIGINT', 'SIGTERM']
+const runner = require('systemic-domain-runner')
 const transports = require('./lib/transports')
 
-domain.run(() => {
-    system.start((err, components) => {
-        if (err) die('Error starting system', err)
-        const logger = components.logger
-
-        process.on('config_reloaded', (config) => scheduleRestart())
-               .on('config_reload_error', (err) => logger.error('Error reloading config', err))
-
-        signals.forEach((signal) => {
-            process.on(signal, () => {
-                logger.info(`Received ${signal}. Attempting to shutdown gracefully.`)
-                system.stop(() => {
-                    process.exit(0);
-                })
-            })
-        })
-
-        domain.on('error', (err) => {
-            logger.error('Unhandled exception. Invoking shutdown', err)
-            system.stop(() => {
-                process.exit(1)
-            })
-        })
-
-        function scheduleRestart() {
-            clearTimeout(scheduleRestart.timeout)
-
-            const delayInHuman = get(components, 'config.service.reload.window') || '60s'
-            const delayInMillis = Math.floor(Math.random() * duration(delayInHuman) / 1000) * 1000
-
-            logger.info(format('Configuration reloaded. Service will restart in %s seconds', delayInMillis / 1000))
-
-            scheduleRestart.timeout = setTimeout(() => {
-                system.restart((err, components) => {
-                    if (err) die('Error restarting system', err)
-                })
-            }, delayInMillis)
-            scheduleRestart.timeout.unref()
-        }
-    })
+runner(system).start((err, components) => {
+    if (err) die('Error starting system', err)
+    const logger = components.logger
+    process.on('confabulous_reload_error', (err) => logger.error('Error reloading config', err))
 })
 
 function die(message, err) {
