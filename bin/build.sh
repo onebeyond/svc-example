31856da73d030cc907caf05725bd147fd725bbbb
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-07-14 23:45:51 +0100

    Configuring build

--- bin/build.sh
+++ bin/build.sh
@@ -16,6 +16,7 @@ fi
 echo Building $IMAGE
 node_modules/.bin/make-manifest --extra "build: $BUILD_NUMBER" --extra "image: $IMAGE"
 docker-compose --file ./docker/docker-compose-build.yml build
+docker-compose --file ./docker/docker-compose-build.yml run -e SERVICE_ENV=build --rm $SERVICE_NAME node_modules/.bin/eslint .
 docker-compose --file ./docker/docker-compose-build.yml run -e SERVICE_ENV=build --rm $SERVICE_NAME node_modules/.bin/mocha tests
 docker tag $REPOSITORY/$SERVICE_NAME:latest $IMAGE
 docker-compose --file ./docker/docker-compose-build.yml down
