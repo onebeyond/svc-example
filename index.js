31b3f9d0760c800f2109dcb2f39bf239886e867f
Stephen Cresswell <git@stephen-cresswell.net>
2017-05-04 13:02:25 +0100

    Stopped swallowing error messages

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const bunyan = require('bunyan')
 const name = require('./package.json').name
 const emergencyLogger = process.env.SERVICE_ENV === 'local' ? console : bunyan.createLogger({ name: name })
 
-runner(system(), { logger: emergencyLogger }).start((err, { logger, pkg }) => {
+runner(system(), { logger: emergencyLogger }).start((err, dependencies) => {
     if (err) die('Error starting system', err)
-    logger.info(`${pkg.name} has started`)
+    dependencies.logger.info(`${dependencies.pkg.name} has started`)
 })
 
 function die(message, err) {
